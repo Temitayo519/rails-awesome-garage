8036b86a3f84b4a404ff70299837217d2781c17d
Temitayo Odubade <temitayo19@hotmail.com>
2024-10-03 17:51:08 +0100

    rails awesome garage

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,4 +2,11 @@ class CarsController < ApplicationController
   def index
     @cars = Car.all
   end
+
+  def show
+    @car = Car.find(params[:id])
+    @review = Review.new
+    @reviews = @car.reviews
+    @favourite = Favourite.new
+  end
 end
